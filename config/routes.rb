5906ab6eb79f87a4544549bf4b890e7050c06a1f
kantaro.sasajima <h27s10k.s@gmail.com>
2020-06-12 12:20:44 +0000

    ユーザの新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/:id', to: "users#show"
   root 'top#index'
+  
+  devise_for :users, controllers: {
+      registrations: 'users/registrations'
+  }
   
 end
