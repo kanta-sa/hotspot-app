e89b5dba9a1ef20104df2da59686f5935350b4c6
kantaro.sasajima <h27s10k.s@gmail.com>
2020-06-17 23:55:20 +0000

    footer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,15 +9,19 @@ CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 # 保存先の分岐
 CarrierWave.configure do |config|
   # 本番環境はS3に保存
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'hotspot-db-app'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hotspot-db-app'
-  config.fog_public = false
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: 'ap-northeast-1' #東京リージョン
-  }
-
+  if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'hotspot-db-app'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hotspot-db-app'
+    config.fog_public = false
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'ap-northeast-1' #東京リージョン
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
