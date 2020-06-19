d847e58975715dce1fefe3e2daf107698d60fdf1
kantaro.sasajima <h27s10k.s@gmail.com>
2020-06-19 13:22:14 +0000

    favorite ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,13 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
   
   def create
+    @post = Post.find(params[:post_id])
     favorite = current_user.favorites.build(post_id: params[:post_id])
     favorite.save
-    flash[:notice] = "お気に入り登録しました"
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     current_user.favorites.find_by(post_id: params[:post_id]).destroy
-    redirect_back(fallback_location: root_path)
   end
 end
