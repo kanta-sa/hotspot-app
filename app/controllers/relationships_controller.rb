8829edd87c96529dc2c18c7e5bfdbfd7c74af26c
kantaro.sasajima <h27s10k.s@gmail.com>
2020-06-19 14:02:32 +0000

    follow ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,12 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    user = User.find(params[:follow_id])
-    current_user.follow(user)
-    flash[:notice] = 'ユーザをフォローしました。'
-    redirect_to user
+    @user = User.find(params[:follow_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    user = User.find(params[:follow_id])
-    current_user.unfollow(user)
-    flash[:notice] = 'ユーザのフォローを解除しました。'
-    redirect_to user
+    @user = User.find(params[:follow_id])
+    current_user.unfollow(@user)
   end
 end
