68f6171c66f4e88ba3df937a77396b196e107dae
kantaro.sasajima <h27s10k.s@gmail.com>
2020-06-21 14:01:07 +0000

    slideにリンク追加

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,16 +1,19 @@
 $(document).on('turbolinks:load', function(){
   $('.slideshow').each(function(){
     let $slides = $(this).find('.slider-image'),
+        $texts = $(this).find('.slider-text'),
         slideCount = $slides.length,
         currentIndex = 0;
     $slides.eq(currentIndex).fadeIn();
+    $texts.eq(currentIndex).fadeIn();
     setInterval(showNextSlide, 7500);
     
     function showNextSlide(){
       let nextIndex = (currentIndex+1) % slideCount;
-      $slides.eq(currentIndex).fadeOut();
-      $slides.eq(nextIndex).fadeIn();
-      
+      $slides.eq(currentIndex).fadeOut(500);
+      $texts.eq(currentIndex).fadeOut(500);
+      $slides.eq(nextIndex).fadeIn(3000);
+      $texts.eq(nextIndex).fadeIn(3000);
       
       currentIndex = nextIndex;
     }
