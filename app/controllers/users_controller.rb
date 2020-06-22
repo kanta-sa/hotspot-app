1a9beb11c7a6fa1544055ac464767af1095ebc21
kantaro.sasajima <h27s10k.s@gmail.com>
2020-06-22 12:07:28 +0000

    following, followerページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_target_user
+  
   def show
-    @user = User.find(params[:id])
     counts(@user)
     @posts = @user.posts.page(params[:page]).per(9)
     @favo_posts = @user.favorite_of_posts.page(params[:page]).per(15)
+  end
+  
+  def followings
+    @followings = @user.followings.page(params[:page])
+  end
+  
+  def followers
+    @followers = @user.followers.page(params[:page])
+  end
+  
+  private
+  
+  def set_target_user
+    @user = User.find(params[:id])
   end
 end
