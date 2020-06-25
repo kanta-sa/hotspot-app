6c212b879a4f597db332c6e0c5d22220d5694db4
kantaro.sasajima <h27s10k.s@gmail.com>
2020-06-25 13:26:56 +0000

    投稿一覧ページの検索エリアのデザイン追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     counts(@user)
     @posts = @user.posts.page(params[:page]).per(9)
-    @favo_posts = @user.favorite_of_posts.page(params[:page]).per(15)
+    @favo_posts = @user.favorite_of_posts.page(params[:page]).per(9)
   end
   
   def follow
