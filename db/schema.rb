2e6a3e8e91705dfe4817dc42d59ed41e6726e61d
kantaro.sasajima <h27s10k.s@gmail.com>
2020-10-08 12:43:51 +0000

    omniauth用のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_015356) do
+ActiveRecord::Schema.define(version: 2020_10_08_123623) do
 
   create_table "cities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "prefecture_id"
@@ -96,6 +96,8 @@ ActiveRecord::Schema.define(version: 2020_06_26_015356) do
     t.string "username"
     t.string "image"
     t.boolean "guest", default: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
