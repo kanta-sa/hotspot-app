9bd63320b5b1b548ca4a11e5fcd1618fb9d4222c
kantaro.sasajima <h27s10k.s@gmail.com>
2020-10-09 12:13:53 +0000

    omniauthコールバック時にnameを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -70,6 +70,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth) # omniauthのコールバック時に呼ばれるメソッド
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.username = auth.info.name
       user.password = Devise.friendly_token[0,20]
     end
   end
