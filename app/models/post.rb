1479a060028a6c177d693e8ffecaca4e5d853ebf
kantaro.sasajima <h27s10k.s@gmail.com>
2020-10-11 09:16:12 +0000

    postモデルにメソッド追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -84,4 +84,20 @@ class Post < ApplicationRecord
     end
     notification.save if notification.valid?
   end
+  
+  def avg_score
+    unless self.reviews.empty?
+      reviews.average(:score).round(1).to_f
+    else
+      0.0
+    end
+  end
+  
+  def review_score_percentage
+    unless self.reviews.empty?
+      reviews.average(:score).round(1).to_f*100/5
+    else
+      0.0
+    end
+  end
 end
