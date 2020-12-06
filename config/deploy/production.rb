b0769a160768dfee281b1a0fc845df540d0d72ae
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2020-12-06 13:53:04 +0900

    circleci上のssh鍵にパスを変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
 server '3.114.25.219', user: 'ec2-user', roles: %w{app db web}
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/hotspotdb.pem'],
+                  keys: ['~/.ssh/id_rsa_f1a17c336403e7811879af82aaf4f0b2'],
                   forward_agent: true
